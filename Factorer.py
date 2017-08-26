a87ff74ebf403934e587c9e1917f9d69733c8593
Deodamnatus <jwgsub@gmail.com>
2017-08-26 10:58:52 -0700

    Add Factorer

---
+++ Factorer.py
@@ -0,0 +1,21 @@
+number = input("**Welcome to the factorer***\n\nEnter a number to factor\n:")
+initialNumber = number
+primeFactorCheck = 2
+incrementNumber = 2
+
+factors = []
+primeList = [2]
+
+def GeneratePrimeList (number, primeList):
+    currentPrime = 2
+
+    # finds primes such that largest prime squared is smaller than number
+    #while number > currentPrime**2:
+
+        #primeList.append(currentPrime)
+
+
+
+# check if number has a factor of prime numbers
+#while number > primeFactorCheck**2:
+    #while primeFactorCheck
